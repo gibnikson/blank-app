937107804d73bec05056a4c646ba802b4bdf4566
Gibson <negibson@ndc.nasa.gov>
2024-09-25 14:32:39 -0700

    check

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests 
 
-main():
     st.title("Welcome to MTS Mobile Micro Manager!")
     st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
