88b00adf25ea97c6e1b8aea7a1efd17aea784e81
Gibson <negibson@ndc.nasa.gov>
2024-09-25 15:51:42 -0700

    URL Added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import requests 
 
 st.title("Welcome to MTS Mobile Micro Manager!")
-st.write(
-"Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+url = st.text_input('Enter the URL to scrape data from', "https://asp-interface.arc.nasa.gov/API/parameter_data/N806NA/PICARD")
+full_packet = requests.get(url);
+data_string = full_packet.text;
+packet_list = data_string.split(",");
+packet_array.append(packet_list);
+st.text(packet_list)
