e2f8cca21b413c6ae7333b96a240054f5f947e17
Gibson <negibson@ndc.nasa.gov>
2024-09-26 14:17:25 -0700

    Main Func

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,14 @@ import requests
 
 ##Variables
 packet_array = [];
+def main():
+    st.title("Welcome to MTS Mobile Micro Manager!")
+    url = st.text_input('Enter the URL to scrape data from', "https://asp-interface.arc.nasa.gov/API/parameter_data/N806NA/PICARD")
+    full_packet = requests.get(url);
+    data_string = full_packet.text;
+    packet_list = data_string.split(",");
+    packet_array.append(packet_list);
+    st.text(packet_list)
 
-st.title("Welcome to MTS Mobile Micro Manager!")
-url = st.text_input('Enter the URL to scrape data from', "https://asp-interface.arc.nasa.gov/API/parameter_data/N806NA/PICARD")
-full_packet = requests.get(url);
-data_string = full_packet.text;
-packet_list = data_string.split(",");
-packet_array.append(packet_list);
-st.text(packet_list)
+if __name__ == "__main__":
+    main()
